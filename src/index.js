5b28fb5c81e69f9250ba925168db9f4432947da7
dharmeshjain52 <dharmeshjain524@gmail.com>
2024-02-29 20:55:50 +0530

    db connectivity and utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+import { app } from './app.js'
 import connectDB from './db/db.js'
 import dotenv from 'dotenv'
+
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR",error)
+        throw error
+    })
+    app.listen(process.env.PORT,()=>{
+        console.log(`SERVER RUNNING AT PORT:${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("DB CONNECTION FAILED")
+})
