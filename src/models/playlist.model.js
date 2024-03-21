799557825326acda3e254f3b1c0c7527a58d9fdb
dharmeshjain52 <dharmeshjain524@gmail.com>
2024-03-21 14:50:59 +0530

    new models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const playlistSchema = new mongoose.Schema(
+{
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    videos:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ]
+},{timestamps:true})
+
+playlistSchema.plugin(mongooseAggregatePaginate)
+
+export const Playlist = mongoose.model("Playlist",playlistSchema)
